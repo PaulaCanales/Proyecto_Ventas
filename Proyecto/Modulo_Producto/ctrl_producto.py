f2ebbd08625768dd8914250c138d93a791f4cd70
gabriela <gabriela>
2015-08-29 19:26:08 -0400

    Buscador

--- Proyecto/Modulo_Producto/ctrl_producto.py
+++ Proyecto/Modulo_Producto/ctrl_producto.py
@@ -19,13 +19,15 @@ class Main(QtGui.QWidget):
         self.load_data()
         self.connect_signals()
         self.filtrar()
-        self.ui.combosku.activated[int].connect(self.onActivated)
+        self.ui.combosku.activated[int].connect(self.onActivated_sku)
+        self.ui.combonombre.activated[int].connect(self.onActivated_nombre)
         self.show()
 
     def connect_signals(self):
         self.ui.agregar.clicked.connect(self.agrega)
         self.ui.eliminar.clicked.connect(self.elimina)
         self.ui.editar.clicked.connect(self.edita)
+        self.ui.buscar.clicked.connect(self.load_data)
 
     def agrega(self):
         self.ui.form = FormProducto(self)
@@ -34,7 +36,7 @@ class Main(QtGui.QWidget):
 
     def load_data(self):
         """
-        Función que carga la información de alumnos en la grilla
+        Función que carga la información de productos en la grilla
         """
         producto = db_model.obtener_producto()
         #Creamos el modelo asociado a la tabla
@@ -56,8 +58,10 @@ class Main(QtGui.QWidget):
         self.data.setHorizontalHeaderItem(
             7, QtGui.QStandardItem(u"Total Ventas"))
 
+
         for r, row in enumerate(producto):
             index = self.data.index(r, 0, QtCore.QModelIndex())
+
             self.data.setData(index, row['sku'])
             index = self.data.index(r, 1, QtCore.QModelIndex())
             self.data.setData(index, row['nombre'])
@@ -121,7 +125,7 @@ class Main(QtGui.QWidget):
 
     def edita(self):
         """
-        Función obtiene el alumno seleccionado en la grilla
+        Función obtiene el producto seleccionado en la grilla
         para poner sus datos en el formulario para su edición
         """
         data = self.ui.grilla_prod.model()
@@ -138,7 +142,7 @@ class Main(QtGui.QWidget):
 
     def filtrar(self):
         """
-        Funcion que agrega todos los sku y nombre al combobox y luego filtra.
+        Funcion que agrega todos los sku y nombre al combobox
         """
         sku= db_model.obtener_sku()
         for dato in range(len(sku)):
@@ -149,8 +153,129 @@ class Main(QtGui.QWidget):
             self.ui.combonombre.addItem(str(nombre[dato][0]))
 
 
-    def onActivated(self, index):
-        sku = self.ui.combosku.itemText(index)
+    def onActivated_sku(self, index1):
+
+        sku = self.ui.combosku.itemText(index1)
+        lista=[]
+        producto = db_model.producto_sku(sku)
+        lista.append(producto)
+        #Creamos el modelo asociado a la tabla
+        self.data = QtGui.QStandardItemModel(len(producto), 7)
+        self.data.setHorizontalHeaderItem(
+            0, QtGui.QStandardItem(u"ID"))
+        self.data.setHorizontalHeaderItem(
+            1, QtGui.QStandardItem(u"Nombre"))
+        self.data.setHorizontalHeaderItem(
+            2, QtGui.QStandardItem(u"Descripcion"))
+        self.data.setHorizontalHeaderItem(
+            3, QtGui.QStandardItem(u"Marca"))
+        self.data.setHorizontalHeaderItem(
+            4, QtGui.QStandardItem(u"Color"))
+        self.data.setHorizontalHeaderItem(
+            5, QtGui.QStandardItem(u"Imagen"))
+        self.data.setHorizontalHeaderItem(
+            6, QtGui.QStandardItem(u"Precio"))
+        self.data.setHorizontalHeaderItem(
+            7, QtGui.QStandardItem(u"Total Ventas"))
+
+
+        for r, row in enumerate(lista):
+        	print r, row
+        	index = self.data.index(r, 0, QtCore.QModelIndex())
+        	self.data.setData(index, row['sku'])
+        	index = self.data.index(r, 1, QtCore.QModelIndex())
+        	self.data.setData(index, row['nombre'])
+        	index = self.data.index(r, 2, QtCore.QModelIndex())
+        	self.data.setData(index, row['descripcion'])
+        	index = self.data.index(r, 3, QtCore.QModelIndex())
+        	self.data.setData(index, row['marca'])
+        	index = self.data.index(r, 4, QtCore.QModelIndex())
+        	self.data.setData(index, row['color'])
+        	index = self.data.index(r, 5, QtCore.QModelIndex())
+        	self.data.setData(index, row['imagen'])
+        	index = self.data.index(r, 6, QtCore.QModelIndex())
+        	self.data.setData(index, row['Precio'])
+        	index = self.data.index(r, 7, QtCore.QModelIndex())
+        	self.data.setData(index, db_model.obtener_TotalProducto(row['sku']))
+
+        self.ui.grilla_prod.setModel(self.data)
+
+        # Para que las columnas 1 y 2 se estire o contraiga cuando
+        # se cambia el tamaño de la pantalla
+        self.ui.grilla_prod.horizontalHeader().setResizeMode(
+            1, self.ui.grilla_prod.horizontalHeader().Stretch)
+        self.ui.grilla_prod.horizontalHeader().setResizeMode(
+            2, self.ui.grilla_prod.horizontalHeader().Stretch)
+
+        self.ui.grilla_prod.setColumnWidth(0, 100)
+        self.ui.grilla_prod.setColumnWidth(1, 210)
+        self.ui.grilla_prod.setColumnWidth(2, 210)
+        self.ui.grilla_prod.setColumnWidth(3, 220)
+        self.ui.grilla_prod.setColumnWidth(4, 220)
+        self.ui.grilla_prod.setColumnWidth(5, 220)
+        self.ui.grilla_prod.setColumnWidth(6, 220)
+
+    def onActivated_nombre(self, index1):
+
+        nom = self.ui.combonombre.itemText(index1)
+        lista=[]
+        producto = db_model.producto_nom(nom)
+        lista.append(producto)
+        #Creamos el modelo asociado a la tabla
+        self.data = QtGui.QStandardItemModel(len(producto), 7)
+        self.data.setHorizontalHeaderItem(
+            0, QtGui.QStandardItem(u"ID"))
+        self.data.setHorizontalHeaderItem(
+            1, QtGui.QStandardItem(u"Nombre"))
+        self.data.setHorizontalHeaderItem(
+            2, QtGui.QStandardItem(u"Descripcion"))
+        self.data.setHorizontalHeaderItem(
+            3, QtGui.QStandardItem(u"Marca"))
+        self.data.setHorizontalHeaderItem(
+            4, QtGui.QStandardItem(u"Color"))
+        self.data.setHorizontalHeaderItem(
+            5, QtGui.QStandardItem(u"Imagen"))
+        self.data.setHorizontalHeaderItem(
+            6, QtGui.QStandardItem(u"Precio"))
+        self.data.setHorizontalHeaderItem(
+            7, QtGui.QStandardItem(u"Total Ventas"))
+
+
+        for r, row in enumerate(lista):
+        	print r, row
+        	index = self.data.index(r, 0, QtCore.QModelIndex())
+        	self.data.setData(index, row['sku'])
+        	index = self.data.index(r, 1, QtCore.QModelIndex())
+        	self.data.setData(index, row['nombre'])
+        	index = self.data.index(r, 2, QtCore.QModelIndex())
+        	self.data.setData(index, row['descripcion'])
+        	index = self.data.index(r, 3, QtCore.QModelIndex())
+        	self.data.setData(index, row['marca'])
+        	index = self.data.index(r, 4, QtCore.QModelIndex())
+        	self.data.setData(index, row['color'])
+        	index = self.data.index(r, 5, QtCore.QModelIndex())
+        	self.data.setData(index, row['imagen'])
+        	index = self.data.index(r, 6, QtCore.QModelIndex())
+        	self.data.setData(index, row['Precio'])
+        	index = self.data.index(r, 7, QtCore.QModelIndex())
+        	self.data.setData(index, db_model.obtener_TotalProducto(row['sku']))
+
+        self.ui.grilla_prod.setModel(self.data)
+
+        # Para que las columnas 1 y 2 se estire o contraiga cuando
+        # se cambia el tamaño de la pantalla
+        self.ui.grilla_prod.horizontalHeader().setResizeMode(
+            1, self.ui.grilla_prod.horizontalHeader().Stretch)
+        self.ui.grilla_prod.horizontalHeader().setResizeMode(
+            2, self.ui.grilla_prod.horizontalHeader().Stretch)
+
+        self.ui.grilla_prod.setColumnWidth(0, 100)
+        self.ui.grilla_prod.setColumnWidth(1, 210)
+        self.ui.grilla_prod.setColumnWidth(2, 210)
+        self.ui.grilla_prod.setColumnWidth(3, 220)
+        self.ui.grilla_prod.setColumnWidth(4, 220)
+        self.ui.grilla_prod.setColumnWidth(5, 220)
+        self.ui.grilla_prod.setColumnWidth(6, 220)
 
 
 if __name__ == '__main__':
